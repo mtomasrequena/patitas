6de19bc14e99c658f68d883cd9e8e20d48c55684
mtomasrequena <m.requenachaves@gmail.com>
2022-12-15 22:52:11 -0300

    inicio del proyecto en remoto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+import { Mascota } from './models/mascota';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'patitas-project';
+  
+  selectedMascota: Mascota = new Mascota();
+
+  mascotaArray: Mascota[] = [
+  { 
+    id: 1, 
+    name:'Teo', 
+    raza: 'Golden', 
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium veniam quam repellendus reprehenderit suscipit laboriosam ipsam quidem amet laudantium totam, alias, explicabo vero ducimus minima quia illo quaerat enim. Magnam?', 
+    img: './assets/imagen1.PNG'
+  },
+    {id: 2, name:'Mile', raza: 'Mestiza', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium veniam quam repellendus reprehenderit suscipit laboriosam ipsam quidem amet laudantium totam, alias?', img: './assets/imagen2.PNG'},
+    {id: 3, name:'Lara', raza: 'Labrador', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium veniam quam repellendus reprehenderit suscipit laboriosam ipsam quidem amet laudantium totam, alias, explicabo vero ducimus minima quia illo quaerat enim. Magnam?', img: './assets/imagen3.jpg'},
+  ];
+
+  openForEdit(mascota: Mascota){
+    this.selectedMascota = mascota;
+  }
+  
+  addOrEdit(){
+    if(this.selectedMascota.id === 0){
+      this.selectedMascota.id = this.mascotaArray.length + 1;
+      this.mascotaArray.push(this.selectedMascota)
+    }
+
+    this.selectedMascota = new Mascota();
+  }
+
+  delete(){
+    if(confirm('¿Estás seguro de queres eliminarlo de la lista?')){
+      this.mascotaArray = this.mascotaArray.filter(x => x != this.selectedMascota);
+      this.selectedMascota = new Mascota();
+    }
+  }
 }
---
+++ src/app/models/mascota.ts
@@ -0,0 +1,7 @@
+export class Mascota {
+    id: number = 0;
+    name?: string;
+    raza?: string;
+    description?: string;
+    img?: string;
+}
